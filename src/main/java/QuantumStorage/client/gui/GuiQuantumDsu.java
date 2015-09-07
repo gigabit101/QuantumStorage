9560e9bb7756d52e919602f0868b912c5c9996c1
gigabit101 <thegigabit101@gmail.com>
2015-09-07 20:15:53 +0100

    Gui now shows stored item amount + the amount in the output slot

--- src/main/java/QuantumStorage/client/gui/GuiQuantumDsu.java
+++ src/main/java/QuantumStorage/client/gui/GuiQuantumDsu.java
@@ -37,7 +37,7 @@ public class GuiQuantumDsu extends GuiContainer {
 		this.fontRendererObj.drawString(I18n.format("container.inventory", new Object[0]), 8, this.ySize - 96 + 2,
 				4210752);
 		this.fontRendererObj.drawString("Amount", 10, 20, 16448255);
-		if (tile.storedItem != null)
-			this.fontRendererObj.drawString(tile.storedItem.stackSize + "", 10, 30, 16448255);
+		if (tile.storedItem != null && tile.getStackInSlot(1) != null)
+			this.fontRendererObj.drawString(tile.storedItem.stackSize + tile.getStackInSlot(1).stackSize + "", 10, 30, 16448255);
 	}
 }
