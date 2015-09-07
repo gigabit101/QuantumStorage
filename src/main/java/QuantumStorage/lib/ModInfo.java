c3cae0e5b037eb3d2784154640d1d2e6d97ad7ee
gigabit101 <thegigabit101@gmail.com>
2015-09-07 20:16:43 +0100

    Version bump 1.2.0

--- src/main/java/QuantumStorage/lib/ModInfo.java
+++ src/main/java/QuantumStorage/lib/ModInfo.java
@@ -3,5 +3,5 @@ package QuantumStorage.lib;
 public class ModInfo {
 	public static final String MOD_ID = "QuantumStorage";
 	public static final String MOD_NAME = "QuantumStorage";
-	public static final String MOD_VERSION = "1.1.0";
+	public static final String MOD_VERSION = "1.2.0";
 }
