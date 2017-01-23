0ee5a112f57d33bba9e775dedfe8db6faecaaf37
gigabit101 <thegigabit101@gmail.com>
2017-01-23 01:36:14 +0000

    Removed test code derp

--- src/main/java/QuantumStorage/tile/TileQuantumTank.java
+++ src/main/java/QuantumStorage/tile/TileQuantumTank.java
@@ -135,7 +135,7 @@ public class TileQuantumTank extends TileQuantumStorage implements IInventory, I
 	{
 		if(capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY)
 		{
-			return (T) new FluidTankPropertiesWrapper(tank);
+			return (T) tank;
 		}
 		if(capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
 		{
