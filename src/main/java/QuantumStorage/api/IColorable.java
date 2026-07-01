5d74d74aea7055ce18d4b46f7d54d1cdb10eae92
Gigabit101 <thegigabit101@gmail.com>
2026-07-02 00:24:20 +0100

    Remove legacy code

--- src/main/java/QuantumStorage/api/IColorable.java
+++
@@ -1,8 +0,0 @@
-package QuantumStorage.api;
-
-import net.minecraft.item.ItemStack;
-
-public interface IColorable
-{
-    int getColorFromItemStack(ItemStack stack, int tintIndex);
-}
--- src/main/java/QuantumStorage/client/keybinding/Key.java
+++
@@ -1,6 +0,0 @@
-package QuantumStorage.client.keybinding;
-
-public enum Key
-{
-    UNKNOWN, CONFIG
-}
